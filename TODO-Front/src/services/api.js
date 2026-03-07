29676aa0b5d1a3261ac2fd2bcdb93ea8346f2c13
jd-lopez <jdll0403@gmail.com>
2026-03-06 19:01:57 -0600

    connected front with back end

--- TODO-Front/src/services/api.js
+++ TODO-Front/src/services/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+
+VITE_API_URL=https://my-todo-k1ak.onrender.com/
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
   withCredentials: false,
